66ef69fcdc9f08fd8ee36004e345b4d7f63fa4bb
NassiHarel <nassi9@gmail.com>
2016-12-09 17:23:39 +0200

    Update connection-pool.js

--- connection-pool.js
+++ connection-pool.js
@@ -1,5 +1,11 @@
 class ConnectionPool {
   constructor(options) {
-   
+    this.connectionPool = new Map();
+    this.redisConfig = options.redisConfig;
+    this.createClientFactory = options.createClientFactory;
+  }
+  
+  createClient()
+  {
   }
 }
