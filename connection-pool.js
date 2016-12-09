7cc087a6e486f279fab41d7cb96da3c582b4abb8
NassiHarel <nassi9@gmail.com>
2016-12-09 17:34:34 +0200

    Update connection-pool.js

--- connection-pool.js
+++ connection-pool.js
@@ -1,11 +1,29 @@
-class ConnectionPool {
+
+var EventEmitter = require('events').EventEmitter;
+const DEFAULT_KEY = 'default_client';
+
+class ConnectionPool extends EventEmitter {
   constructor(options) {
+    super();
     this.connectionPool = new Map();
     this.redisConfig = options.redisConfig;
     this.createClientFactory = options.createClientFactory;
+    this.poolName = options.poolName;
   }
   
-  createClient()
+  createClient(options)
+  {
+    let key = options.key || DEFAULT_KEY;
+    let pool = this.connectionPool.get(key);
+    if(!pool)
+    {
+      this._createClient();
+    }
+    return pool;
+  }
+  
+  _createClient()
   {
   }
+  
 }
