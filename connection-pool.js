6b323f1709df3a4a6dac483d181cf98964d33992
NassiHarel <nassi9@gmail.com>
2016-12-10 13:24:36 +0200

    Update connection-pool.js

--- connection-pool.js
+++ connection-pool.js
@@ -1,19 +1,22 @@
 
 var EventEmitter = require('events').EventEmitter;
-var redis = require('redis')
-const DEFAULT_KEY = 'default_client';
+var redis = require("redis");
+const DEFAULT_CLIENT = 'default_client';
+const DEFAULT_POOL = 'default_pool';
 
 class ConnectionPool extends EventEmitter {
   constructor(options) {
     super();
     this.connectionPool = new Map();
-    this.redisConfig = options.redisConfig;
-    this.createClientFactory = options.createClientFactory;
-    this.poolName = options.poolName;
+  
   }
   
-  createClient(options)
-  {
+  createPool(options){
+      let poolName = options.poolName || DEFAULT_POOL;
+      this.connectionPool.set(poolName, options);
+  }
+  
+  createClient(options){
     let key = options.key || DEFAULT_KEY;
     let pool = this.connectionPool.get(key);
     if(!pool)
@@ -23,18 +26,21 @@ class ConnectionPool extends EventEmitter {
     return pool;
   }
   
-  _createClient(key)
-  {
+  _createClient(key){
     if(typeof this.createClientFactory === 'function'){
       let client = this.createClientFactory();
       if(client)
       {
         this.connectionPool.set(key, client);
       }
+      else
+      {
+        
+      }
     }
     else
     {
-      
+      client = redis.createClient(this.redisConfig.port, this.redisConfig.host, {});
     }
     
   }
